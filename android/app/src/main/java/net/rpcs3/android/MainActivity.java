072ed758a7b0fa7d8d3462ddcd202aace996b924
proqaz54-dev <proqaz54-dev@users.noreply.github.com>
2026-08-17 14:12:19 +0300

    android: bundle SDLActivity framework (org.libsdl.app), MainActivity extends it with rpcs3_android as single library

--- android/app/src/main/java/net/rpcs3/android/MainActivity.java
+++ android/app/src/main/java/net/rpcs3/android/MainActivity.java
@@ -1,73 +1,30 @@
 package net.rpcs3.android;
 
-import android.app.Activity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.FrameLayout;
 
-public class MainActivity extends Activity implements SurfaceHolder.Callback
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity
 {
 	private static final String TAG = "RPCS3";
 
-	private SurfaceView surfaceView;
-
 	private native int nativeBoot(String bootPath);
 
+	// All SDL and RPCS3 code lives inside librpcs3_android.so; the default
+	// SDLActivity would look for separate "SDL3"/"main" libraries.
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
+	protected String[] getLibraries()
 	{
-		super.onCreate(savedInstanceState);
-		surfaceView = new SurfaceView(this);
-		surfaceView.getHolder().addCallback(this);
-		setContentView(new FrameLayout(this) {{
-			addView(surfaceView);
-		}});
-		System.loadLibrary("rpcs3_android");
+		return new String[] { "rpcs3_android" };
 	}
 
+	// Entry point on the SDL thread. SDLActivity's default main() would
+	// look up an SDL_main symbol in the shared library, which this project
+	// does not provide; boot the RPCS3 core directly instead.
 	@Override
-	public void surfaceCreated(SurfaceHolder holder)
+	protected void main()
 	{
-		Log.i(TAG, "surfaceCreated");
-		NativeBridge.setSurface(holder.getSurface());
-		startEmulation();
-	}
-
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
-	{
-		Log.i(TAG, "surfaceChanged: " + width + "x" + height);
-	}
-
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder)
-	{
-		Log.i(TAG, "surfaceDestroyed");
-		NativeBridge.setSurface(null);
-	}
-
-	private void startEmulation()
-	{
-		String bootPath = null; // TODO: file picker -> game path
 		Log.i(TAG, "nativeBoot");
-		nativeBoot(bootPath);
-	}
-}
-
-class NativeBridge
-{
-	private static volatile Surface surface;
-
-	static Surface getSurface()
-	{
-		return surface;
-	}
-
-	static void setSurface(Surface s)
-	{
-		surface = s;
+		nativeBoot(null);
 	}
 }
